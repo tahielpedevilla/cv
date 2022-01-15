35161f519654f3162f871cf28dacb65c80dc7f0d
tahielpedevilla <tahielpedevilladedonato@gmail.com>
2022-01-15 16:11:19 -0300

    Update rounded prop and change link to pdf

--- src/components/ui/ButtonGroup.jsx
+++ src/components/ui/ButtonGroup.jsx
@@ -2,35 +2,40 @@ import React from "react";
 import {FiMail} from "react-icons/fi";
 import {FaLinkedin} from "react-icons/fa";
 import {AiOutlineGithub, AiOutlineWhatsApp} from "react-icons/ai";
-import {VscFilePdf} from "react-icons/vsc";
+import {MdFileDownload} from "react-icons/md";
 import {HStack, IconButton, Tooltip, Link} from "@chakra-ui/react";
+
 const ButtonGroup = () => {
   return (
     <>
       <HStack spacing={4}>
-        <Tooltip fontSize="sm" label="Mail">
+        <Tooltip fontSize="sm" label="E-mail">
           <Link isExternal href="mailto:tahielpeed@gmail.com">
-            <IconButton icon={<FiMail />} rounded="100%" size="lg" variant="solid" />
+            <IconButton borderRadius="full" icon={<FiMail />} size="md" variant="solid" />
           </Link>
         </Tooltip>
         <Link isExternal href="https://www.linkedin.com/in/tahiel-pedevilla-de-donato">
           <Tooltip fontSize="sm" label="LinkedIn">
-            <IconButton icon={<FaLinkedin />} rounded="100%" size="lg" variant="solid" />
+            <IconButton borderRadius="full" icon={<FaLinkedin />} size="md" variant="solid" />
           </Tooltip>
         </Link>
         <Link isExternal href="https://github.com/tahielpedevilla">
           <Tooltip fontSize="sm" label="GitHub">
-            <IconButton icon={<AiOutlineGithub />} rounded="100%" size="lg" variant="solid" />
+            <IconButton borderRadius="full" icon={<AiOutlineGithub />} size="md" variant="solid" />
           </Tooltip>
         </Link>
         <Link isExternal href="https://wa.me/+5492664405142">
           <Tooltip fontSize="sm" label="WhatsApp">
-            <IconButton icon={<AiOutlineWhatsApp />} rounded="100%" size="lg" variant="solid" />
+            <IconButton borderRadius="full" icon={<AiOutlineWhatsApp />} size="md" variant="solid" />
           </Tooltip>
         </Link>
-        <Link isExternal href="https://drive.google.com/file/d/1awqcMegaNXtzHIhpe8ThK3b-1KABJKNF/view?usp=sharing">
-          <Tooltip fontSize="sm" label="CV .pdf">
-            <IconButton icon={<VscFilePdf />} rounded="100%" size="lg" variant="solid" />
+        <Link
+          isExternal
+          href="https://drive.google.com/file/d/1KBJklxvLgr2nmuaiy30FgietNpfzGORi/view?usp=sharing"
+          target="_blank"
+        >
+          <Tooltip fontSize="sm" label="Descargar CV">
+            <IconButton borderRadius="full" icon={<MdFileDownload />} size="md" variant="solid" />
           </Tooltip>
         </Link>
       </HStack>
