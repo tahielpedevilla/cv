4c145b8faa7b928e54ba8d9f62201ce77f970fef
tahielpedevilla <tahielpedevilladedonato@gmail.com>
2022-01-15 16:08:01 -0300

    Update fonts

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,10 +1,11 @@
 import {extendTheme} from "@chakra-ui/react";
+
 const theme = extendTheme({
   styles: {
     global: {
       fonts: {
-        heading: "Poppins",
-        body: "Poppins",
+        heading: "Inter, sans-serif",
+        body: "Inter, sans-serif",
       },
     },
   },
